ca4f2a96f613e3c51d560e3bb8400261b2229562
unknown <pedro.cruz@macro.com.br>
2020-11-11 00:27:05 -0300

    atualizandoooo

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import styles from '../../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-    <h1> Atualizei</h1>
+    <h1> Atualizeiiii</h1>
     </div>
   )
 }
